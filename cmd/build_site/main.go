f6e21f4d5c34cb3ffa59314c6f8512ad8dc17f2e
Meirbek <53935454+MeirbekAshirbayev@users.noreply.github.com>
2025-12-19 17:29:28 +0500

    Change basePath to root domain

--- cmd/build_site/main.go
+++ cmd/build_site/main.go
@@ -15,7 +15,7 @@ func main() {
 	cwd, _ := os.Getwd()
 	fmt.Printf("Building site to 'docs' in %s\n", cwd)
 
-	if err := builder.BuildSite("docs", "/mysitecheck"); err != nil {
+	if err := builder.BuildSite("docs", "/"); err != nil {
 		log.Fatalf("Build failed: %v", err)
 	}
 	fmt.Println("Build success!")
